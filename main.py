9c887da6b657b62d14668ffbbfe5f788bd402594
meskintomer <meskintomer@gmail.com>
2024-03-02 17:27:49 +0200

    Documentation and finished exercise

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+Author: Tomer Meskin
+Date: 01/03/24
+Checks which ports are in a LISTENING state in a specific IP
+"""
+
 from scapy import *
 from scapy.layers.inet import *
 
@@ -8,18 +14,27 @@ TIMEOUT = 0.5
 
 
 def check(port):
+    """
+    Sends a SYN packet to a given port and checks if the port responses - the port is open
+    :param port: The intended PORT to check
+    :return: None
+    """
     # Construct packets
     ip = IP(dst=TARGET_IP)
     syn = TCP(dport=port, flags='S')
     # Send syn and sniff response
     response = sr1(ip/syn, timeout=TIMEOUT, verbose=0)
 
+    # No response
     if response is None:
         print(".", end="")
+    # Response has RST flag
     elif TCP in response and response[TCP].flags & 0x04:
         print("-", end="")
+    # Response has TCP layer and SYN+ACK flags
     elif TCP in response and response[TCP].flags & 0x12:
         print(f"Port {port} is available")
+    # Otherwise
     else:
         print(f"Port {port} status is unknown")
 
